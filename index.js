db570fc7b7ee03daf2ab2ae937f855d26004ee91
Jack <jack.m.dowling26@gmail.com>
2022-03-04 17:31:16 -0500

    Errors in parameters are handled, started work on successful requests

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
@@ -19,9 +20,12 @@ app.get("/api/ping", (req, res) => {
 });
 
 // Route 2
-app.get("/api/posts", (req, res, next) => {
-  // Destructuring query string
+app.get("/api/posts", async (req, res) => {
+  // Destructuring query string, declaring variables
   let { tag, sortBy, direction } = req.query;
+  const validSort = ["id", "reads", "likes", "popularity"];
+  const validDir = ["asc", "desc"];
+
   // Error handling for bad params
   if (!tag) {
     const missingTag = {
@@ -30,8 +34,35 @@ app.get("/api/posts", (req, res, next) => {
     res.status(400);
     res.json(missingTag);
   }
+  if (sortBy && !validSort.includes(sortBy)) {
+    const sortError = {
+      error: `sortBy parameter is invalid`,
+    };
+    res.status(400);
+    res.json(sortError);
+  }
+  if (direction && !validDir.includes(direction)) {
+    const dirError = {
+      error: `Direction parameter is invalid`,
+    };
+    res.status(400);
+    res.json(dirError);
+    // This wasn't super DRY, but it would have been a messy if statement to do it with a var and template literals
+  }
 
+  // Successful requests
+  if (!sortBy) {
+    sortBy = "id";
+  }
+  if (!direction) {
+    direction = "asc";
+  }
+  const tags = tag.split(",");
+  const request = tags.map((tag) => {
+    axios.get(url + "?tag=" + tag + sortBy + direction);
+  });
 
-  console.log(tag);
+  console.log(tags);
+
   res.send(tag);
 });
