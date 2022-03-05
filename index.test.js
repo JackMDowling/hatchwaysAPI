d9eb17ae0f7d96971e08bfcf478d09ec535882ec
Jack <jack.m.dowling26@gmail.com>
2022-03-04 20:09:48 -0500

    pseudocoding test file

--- index.test.js
+++ index.test.js
@@ -2,7 +2,13 @@ import supertest from "supertest";
 import app from "./index";
 
 // test the ping route
+  // should get status 200
+  // reponse should be json
 
 // test the post route
+  // should get error 400 if no tag
+  // should get error 400 if sort/dir are bad
+  // should get 200 if params are good
+  // could test ascend and descend to see if they work
 
 // test non-routes
