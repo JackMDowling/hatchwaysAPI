849eb58765f7010f7076a5fc5ee2f480a380f660
Jack <jack.m.dowling26@gmail.com>
2022-03-07 11:23:33 -0500

    test suite done

--- index.test.js
+++ index.test.js
@@ -13,10 +13,9 @@ describe("Hitting the ping route", () => {
     });
   });
 });
-// should get status 200
-// response should be json
 
 // test the post route
+// 400 error tests
 describe("Hitting the posts route", () => {
   describe("When no tag parameter is included", () => {
     describe("Upon sending a get request to the post route with no tag", () => {
@@ -25,7 +24,6 @@ describe("Hitting the posts route", () => {
         expect(response.statusCode).toBe(400);
       });
     });
-    // should get error 400 if no tag
   });
   describe("When sortBy is not valid", () => {
     describe("Upon sending a get request to the post route with an invalid sortBy", () => {
@@ -36,7 +34,6 @@ describe("Hitting the posts route", () => {
         expect(response.statusCode).toBe(400);
       });
     });
-    // should get error 400 if sort/dir are bad
   });
   describe("When direction is not valid", () => {});
   describe("Upon sending a get request to the post route with an invalid direction", () => {
@@ -55,14 +52,60 @@ describe("Hitting the posts route", () => {
         expect(response.statusCode).toBe(200);
       });
     });
-    // should get 200 if params are good
+    test("should send json response", async () => {
+      const response = await request(app).get("/api/posts/?tag=science,tech");
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("json")
+      );
+    });
+    test("should contain posts in the response body", async () => {
+      const response = await request(app).get("/api/posts/?tag=science,tech");
+      expect(response.body.posts).toBeDefined();
+    });
   });
-  describe("When ascending order is selected", () => {
-    // sortBy value should be the lowest
+});
+describe("When ascending order is selected", () => {
+  test("each ID value should be lower than the next when direction is ascending", async () => {
+    const response = await request(app).get(
+      "/api/posts/?tag=science,tech&sortBy=id&direction=asc"
+    );
+    let { posts } = response.body;
+    let firstID = posts[0].id;
+    let allIDs = [];
+    let boolTest = true;
+    for (let post of posts) {
+      allIDs.push(post.id);
+    }
+    for (let i = 1; i < allIDs.length; i++) {
+      if (firstID > allIDs[i]) {
+        boolTest = false;
+      }
+      firstID = allIDs[i];
+    }
+    expect(boolTest).toEqual(true);
   });
-  describe("When descending order is selected", () => {
-    // sortBy value should be the highest
+
+  // sortBy value should be the lowest
+});
+describe("When descending order is selected", () => {
+  test("each ID value should be lower than the next when direction is ascending", async () => {
+    const response = await request(app).get(
+      "/api/posts/?tag=science,tech&sortBy=id&direction=desc"
+    );
+    let { posts } = response.body;
+    let firstID = posts[0].id;
+    let allIDs = [];
+    let boolTest = true;
+    for (let post of posts) {
+      allIDs.push(post.id);
+    }
+    for (let i = 1; i < allIDs.length; i++) {
+      if (firstID < allIDs[i]) {
+        boolTest = false;
+      }
+      firstID = allIDs[i];
+    }
+    expect(boolTest).toEqual(true);
   });
 });
 
-// test non-routes
